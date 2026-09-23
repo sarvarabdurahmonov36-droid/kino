45ceee648182107ec39a295cfdc03e2ad7382073
sarvarabdurahmonov36-droid <sarvarabdurahmonov36@gmail.com>
2026-09-23 20:44:14 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8886749050:AAEAwE8OQ_rnqnAn_8BeCNymISlvsysI8nQ")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6297501997"))
+DATABASE_URL = os.getenv("DATABASE_URL", "")
